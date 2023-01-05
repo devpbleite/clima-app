69a78bac236f2844bc8e15f26ae1e5d62358497c
Pablo Leite <pabloleite03@gmail.com>
2023-01-05 15:00:08 -0300

    Upload the project

---
+++ src/App.jsx
@@ -0,0 +1,216 @@
+import { useRef, useState, useEffect } from "react";
+
+const Api_key = "aaf9a304e82f54775862b9d8999be12f";
+
+const App = () => {
+  const inputRef = useRef(null);
+  const [apiData, setApiData] = useState(null);
+  const [showWeather, setShowWeather] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+
+  const WeatherTypes = [
+    {
+      type: "Clear",
+      img: "https://cdn-icons-png.flaticon.com/512/6974/6974833.png",
+    },
+    {
+      type: "Rain",
+      img: "https://cdn-icons-png.flaticon.com/512/3351/3351979.png",
+    },
+    {
+      type: "Snow",
+      img: "https://cdn-icons-png.flaticon.com/512/642/642102.png",
+    },
+    {
+      type: "Clouds",
+      img: "https://cdn-icons-png.flaticon.com/512/414/414825.png",
+    },
+    {
+      type: "Haze",
+      img: "https://cdn-icons-png.flaticon.com/512/1197/1197102.png",
+    },
+    {
+      type: "Smoke",
+      img: "https://cdn-icons-png.flaticon.com/512/4380/4380458.png",
+    },
+    {
+      type: "Mist",
+      img: "https://cdn-icons-png.flaticon.com/512/4005/4005901.png",
+    },
+    {
+      type: "Drizzle",
+      img: "https://cdn-icons-png.flaticon.com/512/3076/3076129.png",
+    },
+  ];
+
+  const fetchWeather = async () => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&units=metric&appid=${Api_key}`;
+    setLoading(true);
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setApiData(null);
+        if (data.cod == 404 || data.cod == 400) {
+          // ARRAY OF OBJ
+          setShowWeather([
+            {
+              type: "Not Found",
+              img: "https://cdn-icons-png.flaticon.com/512/4275/4275497.png",
+            },
+          ]);
+        }
+        setShowWeather(
+          WeatherTypes.filter(
+            (weather) => weather.type === data.weather[0].main
+          )
+        );
+        console.log(data);
+        setApiData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') ? localStorage.getItem('theme') : "system");
+  const element = document.documentElement;
+  const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+  const options = [
+    {
+      icon: "sunny",
+      text: "light"
+    },
+    {
+      icon: "moon",
+      text: "dark"
+    },
+    {
+      icon: "desktop-outline",
+      text: "system"
+    }
+  ];
+
+  function onWindowMatch() {
+    if (localStorage.theme === "dark" || (!("theme" in localStorage) && darkQuery.matches))
+    {
+      element.classList.add("dark");
+    } else {
+      element.classList.remove("dark")
+    }
+  }
+  onWindowMatch();
+
+  useEffect(() => {
+    switch (theme) {
+      case "dark":
+        element.classList.add("dark");
+        localStorage.setItem('theme', 'dark');
+        break;
+      case "light":
+        element.classList.remove("dark");
+        localStorage.setItem('theme', 'light')
+        break;
+      default:
+        localStorage.removeItem('theme');
+        onWindowMatch()
+        break;
+    }
+  }, [theme]);
+
+  darkQuery.addEventListener("change", (e) => {
+    if (!("theme" in localStorage)) {
+      if (e.matches) {
+        element.classList.add('dark');
+      } else {
+        element.classList.remove('dark');
+      }
+    }
+  })
+
+  return (
+    <div className="min-h-screen p-8 grid place-items-center bg-slate-100 dark:bg-slate-900 duration-100">
+      <div className="fixed top-5 right-10 dark:text-slate-100 duration-100 dark:bg-slate-700 bg-gray-200 rounded">
+        {options?.map((opt) => (
+            <button key={opt.text}
+             onClick={() => setTheme(opt.text)}
+             className={`w-8 h-8 leading-9 text-xl rounded-full m-1
+             ${theme === opt.text && "text-sky-500"}`}>
+            <ion-icon name={opt.icon}></ion-icon>
+        </button>
+          ))}
+      </div>
+      <div className="bg-white w-96 p-4 rounded-md border-2">
+        <div className="flex items-center justify-between">
+          <input
+            type="text"
+            ref={inputRef}
+            placeholder="Enter Your Location"
+            className="text-xl border-b rounded-md
+          p-1 border-gray-200 font-semibold uppercase flex-1"
+          />
+          <button onClick={fetchWeather}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/758/758651.png"
+              alt="..."
+              className="w-8"
+            />
+          </button>
+        </div>
+        <div
+          className={`duration-300 delay-75  overflow-hidden
+         ${showWeather ? "h-[27rem]" : "h-0"}`}
+        >
+          {loading ? (
+            <div className="grid place-items-center h-full">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/1477/1477009.png"
+                alt="..."
+                className="w-14 mx-auto mb-2 animate-spin"
+              />
+            </div>
+          ) : (
+            showWeather && (
+              <div className="text-center flex flex-col gap-6 mt-10">
+                {apiData && (
+                  <p className="text-xl font-semibold">
+                    {apiData?.name + "," + apiData?.sys?.country}
+                  </p>
+                )}
+                <img
+                  src={showWeather[0]?.img}
+                  alt="..."
+                  className="w-52 mx-auto"
+                />
+                <h3 className="text-2xl font-bold text-zinc-800">
+                  {showWeather[0]?.type}
+                </h3>
+
+                {apiData && (
+                  <>
+                    <div className="flex justify-center">
+                      <img
+                        src="https://cdn-icons-png.flaticon.com/512/7794/7794499.png"
+                        alt="..."
+                        className="h-9 mt-1"
+                      />
+                      <h2 className="text-4xl font-extrabold">
+                        {apiData?.main?.temp}&#176;C
+                      </h2>
+                    </div>
+                  </>
+                )}
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: "Poppins",
+      },
+    },
+  },
+  plugins: [],
+};
